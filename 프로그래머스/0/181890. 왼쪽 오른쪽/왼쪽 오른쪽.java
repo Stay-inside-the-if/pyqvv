d81c6876ea22e766d5abd215b67167bfdc8f4764
신서영 <pyqw_@naver.com>
2025-04-28 01:00:44 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -2,16 +2,20 @@ class Solution {
     public String[] solution(String[] str_list) {
         int str_len = str_list.length;
         String[] answer = null;
+        
         for (int i = 0; i < str_len; i++) {
             if (str_list[i].equals("l")) {
                 answer = new String[i];
+                
                 for (int j = 0; j < i; j++) {
-                        answer[j] = str_list[j];   
+                    answer[j] = str_list[j];   
                 }
                 return answer;
             }
-            if (str_list[i].equals("r") && (i < (str_len - 1))) {
+            
+            if (str_list[i].equals("r")) {
                 answer = new String[str_len - i - 1];
+                
                 for (int j = 0; j < str_len - i - 1; j++) {
                     answer[j] = str_list[i+j+1];
                 }
