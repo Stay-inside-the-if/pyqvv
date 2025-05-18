ad2eb8551685f0f879b13bb8ad04e31fc41026ba
신서영 <pyqw_@naver.com>
2025-05-19 00:36:31 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -3,22 +3,15 @@ class Solution {
         int lastNum = common[common.length - 1];
         int num = 0;
         
-        for (int i = 0; ;i++) {
-            if ((common[i] != 0) && (common[i+1] != 0) && (common[i+2] != 0)) {
-                num = common[i+1] / common[i];
-                
-                if ((num == (common[i+2] / common[i+1])) && ((common[i+1] % common[i]) == 0)) {
-                    return (lastNum * num);
-                }  
-                else {
-                    num = common[i+1] - common[i];
-                    return (lastNum + num);
-                }
+        if ((common[0] != 0) && (common[1] != 0) && (common[2] != 0)) {
+            num = common[1] / common[0];
+            
+            if (((common[1] % common[0]) == 0) && (num == (common[2] / common[1]))) {
+                return lastNum * num;
             }
-            else {
-                num = common[i+1] - common[i];
-                return (lastNum + num);
-            }
-        }      
+        }
+        
+        num = common[1] - common[0];
+        return lastNum + num;
     }
 }
